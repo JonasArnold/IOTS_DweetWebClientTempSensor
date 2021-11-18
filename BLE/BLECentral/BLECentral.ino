05e84425fae067826db84a18d9f250df38d64bd0
Simon Frei <simon-frei@hotmail.com>
2021-11-18 19:09:03 +0100

    add Central unbeauty

--- BLE/BLECentral/BLECentral.ino
+++ BLE/BLECentral/BLECentral.ino
@@ -1,14 +1,12 @@
 /*
-  LED Control
+  Peripheral Explorer
 
-  This example scans for BLE peripherals until one with the advertised service
-  "19b10000-e8f2-537e-4f6c-d104768a1214" UUID is found. Once discovered and connected,
-  it will remotely control the BLE Peripheral's LED, when the button is pressed or released.
+  This example scans for BLE peripherals until one with a particular name ("LED")
+  is found. Then connects, and discovers + prints all the peripheral's attributes.
 
   The circuit:
   - Arduino MKR WiFi 1010, Arduino Uno WiFi Rev2 board, Arduino Nano 33 IoT,
     Arduino Nano 33 BLE, or Arduino Nano 33 BLE Sense board.
-  - Button with pull-up resistor connected to pin 2.
 
   You can use it with another board that is compatible with this library and the
   Peripherals -> LED example.
@@ -18,24 +16,23 @@
 
 #include <ArduinoBLE.h>
 
-// variables for button
-const int buttonPin = 2;
-int oldButtonState = LOW;
-
+int illuminanceState = 0;
+int oldIlluminanceState = 0;
 void setup() {
   Serial.begin(9600);
   while (!Serial);
 
-  // configure the button pin as input
-  pinMode(buttonPin, INPUT);
+  // begin initialization
+  if (!BLE.begin()) {
+    Serial.println("starting BLE failed!");
 
-  // initialize the BLE hardware
-  BLE.begin();
+    while (1);
+  }
 
-  Serial.println("BLE Central - LED control");
+  Serial.println("BLE Central - IOTS");
 
   // start scanning for peripherals
-  BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
+  BLE.scan();
 }
 
 void loop() {
@@ -52,21 +49,17 @@ void loop() {
     Serial.print(peripheral.advertisedServiceUuid());
     Serial.println();
 
-    if (peripheral.localName() != "LED") {
-      return;
+    // see if peripheral is a LED
+    if (peripheral.localName() == "IOTS_BLE_Peripheral_JoSi") {  //Local Name hoes here
+      // stop scanning
+      BLE.stopScan();
+
+      explorerPeripheral(peripheral);
     }
-
-    // stop scanning
-    BLE.stopScan();
-
-    controlLed(peripheral);
-
-    // peripheral disconnected, start scanning again
-    BLE.scanForUuid("19b10000-e8f2-537e-4f6c-d104768a1214");
   }
 }
 
-void controlLed(BLEDevice peripheral) {
+void explorerPeripheral(BLEDevice peripheral) {
   // connect to the peripheral
   Serial.println("Connecting ...");
 
@@ -77,52 +70,120 @@ void controlLed(BLEDevice peripheral) {
     return;
   }
 
-  // discover peripheral attributes
-  Serial.println("Discovering attributes ...");
-  if (peripheral.discoverAttributes()) {
-    Serial.println("Attributes discovered");
-  } else {
-    Serial.println("Attribute discovery failed!");
-    peripheral.disconnect();
-    return;
-  }
+  while (peripheral) {
+    // discover peripheral attributes
+    Serial.println("Discovering attributes ...");
+    if (peripheral.discoverAttributes()) {
+      Serial.println("Attributes discovered");
+    } else {
+      Serial.println("Attribute discovery failed!");
+      peripheral.disconnect();
+      return;
+    }
 
-  // retrieve the LED characteristic
-  BLECharacteristic ledCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
+    BLECharacteristic temperatureCharacteristic = peripheral.characteristic("2A6E");
+    BLECharacteristic humidityCharacteristic = peripheral.characteristic("2A6F");
+    BLECharacteristic illuminanceCharacteristic = peripheral.characteristic("2AFB");
+    BLECharacteristic ledCharacteristic = peripheral.characteristic("19b10001-e8f2-537e-4f6c-d104768a1214");
 
-  if (!ledCharacteristic) {
-    Serial.println("Peripheral does not have LED characteristic!");
-    peripheral.disconnect();
-    return;
-  } else if (!ledCharacteristic.canWrite()) {
-    Serial.println("Peripheral does not have a writable LED characteristic!");
-    peripheral.disconnect();
-    return;
-  }
+    if(!illuminanceCharacteristic){
+      Serial.println('no illuminanceCharacteristic');
+      peripheral.disconnect();
+      return;
+    }else if(!illuminanceCharacteristic.canSubscribe()){
+      Serial.println('cant subscribe illuminanceCharacteristic');
+      peripheral.disconnect();
+      return;            
+    }else if(!illuminanceCharacteristic.subscribe()){
+      Serial.println("subscription of illuminanceCharacteristic failed!");
+      peripheral.disconnect();
+      return;
+    }else{
+      Serial.println('subscribe illuminanceCharacteristic success');
+    }
 
-  while (peripheral.connected()) {
-    // while the peripheral is connected
+    if(!humidityCharacteristic){
+      Serial.println('no humidityCharacteristic');
+      peripheral.disconnect();
+      return;
+    }else if(!humidityCharacteristic.canSubscribe()){
+      Serial.println('cant subscribe humidityCharacteristic');
+      peripheral.disconnect();
+      return;            
+    }else if(!humidityCharacteristic.subscribe()){
+      Serial.println("subscription of humidityCharacteristic failed!");
+      peripheral.disconnect();
+      return;
+    }else{
+      Serial.println('subscribe humidityCharacteristic success');
+    }
 
-    // read the button pin
-    int buttonState = digitalRead(buttonPin);
+    if(!temperatureCharacteristic){
+      Serial.println('no temperatureCharacteristic');
+      peripheral.disconnect();
+      return;
+    }else if(!temperatureCharacteristic.canSubscribe()){
+      Serial.println('cant subscribe temperatureCharacteristic');
+      peripheral.disconnect();
+      return;            
+    }else if(!temperatureCharacteristic.subscribe()){
+      Serial.println("subscription of temperatureCharacteristic failed!");
+      peripheral.disconnect();
+      return;
+    }else{
+      Serial.println('subscribe temperatureCharacteristic success');
+    }
 
-    if (oldButtonState != buttonState) {
+    if (!ledCharacteristic) {
+      Serial.println("Peripheral does not have LED characteristic!");
+      peripheral.disconnect();
+      return;
+    } else if (!ledCharacteristic.canWrite()) {
+      Serial.println("Peripheral does not have a writable LED characteristic!");
+      peripheral.disconnect();
+      return;
+    }
+    int16_t illVal, humVal, tempVal;
+    while(peripheral.connected()){
+      if(illuminanceCharacteristic.valueUpdated()){
+        illuminanceCharacteristic.readValue(illVal);
+        Serial.print("illuminance: ");
+        Serial.println(illVal);    
+      }
+      if(humidityCharacteristic.valueUpdated()){
+        humidityCharacteristic.readValue(humVal);
+        Serial.print("humidity: ");
+        Serial.println(humVal);    
+      }
+      if(temperatureCharacteristic.valueUpdated()){
+        temperatureCharacteristic.readValue(tempVal);
+        Serial.print("temp: ");
+        Serial.println(tempVal);    
+      }    
+
+      if(illVal >= 100){
+        illuminanceState = 1;                
+      }else{
+        illuminanceState = 0;
+      }
+
+     if (oldIlluminanceState != illuminanceState) {
       // button changed
-      oldButtonState = buttonState;
+      oldIlluminanceState = illuminanceState;
 
-      if (buttonState) {
-        Serial.println("button pressed");
-
-        // button is pressed, write 0x01 to turn the LED on
+      if (illuminanceState) {
+        Serial.println("ill > 100");
         ledCharacteristic.writeValue((byte)0x01);
       } else {
-        Serial.println("button released");
-
-        // button is released, write 0x00 to turn the LED off
+        Serial.println("ill < 100");
         ledCharacteristic.writeValue((byte)0x00);
       }
-    }
+     }
+    delay(100);
+    
   }
-
-  Serial.println("Peripheral disconnected");
+  }
 }
+
+
+
